3e3a62628ea7279a48d2ef01c4a0c404b49a3032
chennakeshava <89085040+chennakeshava-marvel@users.noreply.github.com>
2023-09-29 21:23:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import header from './header/header';
+import body  from'./body/body';
+import footer from './footer/footer';
+
+const App = () => {
+  return (
+    <div>
+  <header />
+  <body />
+ <footer />
+    </div>
+  )
+}
+
+export default App
